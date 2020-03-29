451be19376d1a181e1423481ad4f3213754f72de
MarcoPadron19 <61167378+MarcoPadron19@users.noreply.github.com>
2020-03-28 22:38:33 -0500

    Player added
    There is now a player with movement and a somewhat pleasing camera movement. I will adjust the camera later.

---
+++ D&D_Dating_Simulator/Assets/Scripts/Timer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float currentTime = 0f;
+    public float startingTime = 10f;
+
+    public Text countDown;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        countDown.text = currentTime.ToString("0");
+
+        if (currentTime <= 0)
+        {
+            currentTime = 0;
+            SceneManager.LoadScene(2);
+        }
+    }
+}
